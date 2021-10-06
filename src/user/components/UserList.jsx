6bf1281deee9d3de82d25e437aaf886bc49bec77
olivia <zhouxuanolivia@gmail.com>
2021-10-06 19:14:54 +1000

    add user component

--- src/user/components/UserList.jsx
+++ src/user/components/UserList.jsx
@@ -1,6 +1,29 @@
 import React from "react";
+import UserItem from "./UserItem";
 
-const UserList = () => {};
+import "./UserList.css";
 
+const UserList = (props) => {
+  console.log(props.users);
+  if (props.users.length === 0) {
+    return (
+      <div className="center">
+        <h2>No users found.</h2>
+      </div>
+    );
+  }
+
+  return props.users.map((user) => (
+    <ul className="users-list">
+      <UserItem
+        key={user.id}
+        id={user.id}
+        name={user.name}
+        img={user.image}
+        placecount={user.places}
+      />
+    </ul>
+  ));
+};
 
 export default UserList;
