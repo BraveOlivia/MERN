37da33e718a74a330c8edf50a1bf8f3dfa2c23fe
olivia <zhouxuanolivia@gmail.com>
2021-10-28 23:11:25 +1000

    add modal css and class

--- src/a.js
+++ src/a.js
@@ -9,3 +9,12 @@ fs.writeFileSync("user-data.txt", "Username" + userName, (err) => {
   }
   console.log("No error");
 });
+
+
+// NodeJS is running JS in non-browser environment, when we what to use some built-in 
+// functions(e.g. alert()), need to import related APIs
+
+// Callback - A function which will be called when the operation is done. 
+// Asynchoronous - when a function takes longer to execute, the complier will not block the execution of the other code.
+
+// CMD to run NodeJS code: `node xxx.js`
