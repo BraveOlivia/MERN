03c2f21380d5e88b732c146131ebab924a82200f
olivia <zhouxuanolivia@gmail.com>
2021-10-29 23:55:16 +1000

    add modal overlay

--- src/shared/components/UIElements/Modal.jsx
+++ src/shared/components/UIElements/Modal.jsx
@@ -1,6 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { CSSTransition } from "react-transition-group";
+
+import Backdrop from "./Backdrop";
 import "./Modal.css";
+
+const ModalOverlay = (props) => {
+  const content = (
+    <div className={`modal ${props.className}`} style={props.style}>
+      <header className={`modal_header ${props.headerClass}`}>
+        <h2>{props.header}</h2>
+      </header>
+    </div>
+  );
+};
 
 const Modal = (props) => {};
 
